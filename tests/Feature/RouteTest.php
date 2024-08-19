0c8e93778d3b4389101c060be92e1823846120fd
Melvin Jones Repol <mrepol742@gmail.com>
2024-08-20 06:38:44 +0800

    added lint & php test

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -3,17 +3,21 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\User;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class RouteTest extends TestCase
 {
     /**
-     * A basic test example.
-     */
+    * Test retrieving the application response
+    *
+    * @return void
+    */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/');
         $response->assertStatus(200);
     }
 }
